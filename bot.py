1b5f7052c203278869bb3a3571ef9649f4c633a1
noir-prog <nickmementomorry@gmail.com>
2026-09-17 01:56:01 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -355,7 +355,7 @@ async def handle_business_message(message):
             f"🆔 Message ID: {message.message_id}\n"
         )
 
-                # ================== PROFILE BUTTONS ==================
+                        # ================== PROFILE BUTTONS ==================
 
         profile_keyboard = InlineKeyboardMarkup(
             inline_keyboard=[
@@ -366,7 +366,7 @@ async def handle_business_message(message):
                     ),
                     InlineKeyboardButton(
                         text="📨 Получатель",
-                        url=f"tg://user?id={business_connection.user.id}",
+                        url=f"tg://user?id={message.chat.id}",
                     ),
                 ]
             ]
