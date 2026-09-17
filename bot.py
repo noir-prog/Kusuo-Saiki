9f24736d50077872c003548b0a4846f83191f5a0
noir-prog <nickmementomorry@gmail.com>
2026-09-17 13:29:22 +0300

    Update bot.py

--- bot.py
+++ bot.py
@@ -171,7 +171,7 @@ async def handle_ui_callback(callback: CallbackQuery):
                     [
                         InlineKeyboardButton(
                             text="⚙️ НАСТРОЙКИ АККАУНТА",
-                            url="tg://settings/business_bots",
+                            url="tg://settings/",
                         )
                     ],
                     [
@@ -334,7 +334,7 @@ async def handle_ui_callback(callback: CallbackQuery):
                         [
                             InlineKeyboardButton(
                                 text="⚙️ НАСТРОЙКИ АККАУНТА",
-                                url="tg://settings/business_bots",
+                                url="tg://settings/",
                             )
                         ],
                         [
